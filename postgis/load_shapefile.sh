3a067549541a1bb7a28e15d23d7eab27cb631a6f
Ghassen Aouinti <ghassen.aouinti@gmail.com>
2024-06-12 17:23:43 -0400

    add postgis import shapefile

---
+++ postgis/load_shapefile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Unzip the shapefile
+unzip /admin_0_countries.zip -d /shapefile
+
+# Wait for the PostgreSQL server to start
+until pg_isready -h localhost -p 5432 -U "$POSTGRES_USER"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+# Load the shapefile into the database using shp2pgsql
+shp2pgsql -I -s 4326 /shapefile/admin_0_countries.shp admin_0_countries | PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DBNAME
