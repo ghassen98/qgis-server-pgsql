47383a111879b22b10d5644c56903bb71a7f3df4
Ghassen Aouinti <ghassen.aouinti@gmail.com>
2024-06-12 19:07:10 -0400

    add pgadmin

--- postgis/load_shapefile.sh
+++ postgis/load_shapefile.sh
@@ -5,10 +5,10 @@ set -e
 unzip /admin_0_countries.zip -d /shapefile
 
 # Wait for the PostgreSQL server to start
-until pg_isready -h localhost -p 5432 -U "$POSTGRES_USER"; do
-  >&2 echo "Postgres is unavailable - sleeping"
+until pg_isready -h postgis -p 5432 -U "$POSTGRES_USER"; do
+  >&2 echo "Postgres is unavailable - sleeping AAA"
   sleep 1
 done
-
+ 
 # Load the shapefile into the database using shp2pgsql
-shp2pgsql -I -s 4326 /shapefile/admin_0_countries.shp admin_0_countries | PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DBNAME
+shp2pgsql -I -s 4326 /shapefile/admin_0_countries/admin_0_countries.shp admin_0_countries | PGPASSWORD=$POSTGRES_PASSWORD psql -h postgis -U $POSTGRES_USER -d $POSTGRES_DBNAME
