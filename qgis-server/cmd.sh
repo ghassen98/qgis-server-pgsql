26947201ae4b450d58a76f73a7467e655cfc89fd
Ghassen Aouinti <ghassen.aouinti@gmail.com>
2024-12-29 21:58:51 -0500

    add qgis project template

--- qgis-server/cmd.sh
+++ qgis-server/cmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Remplace les variables d'environnement dans le fichier template
+envsubst < /data/world_template.qgs > /data/world.qgs
+
 [[ $DEBUG == "1" ]] && env
 
 exec /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
