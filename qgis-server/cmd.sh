eb9d9e53b15868cdace982f5898addc323959bce
Ghassen Aouinti <ghassen.aouinti@gmail.com>
2025-03-26 17:22:27 -0400

    add layers and symbols

--- qgis-server/cmd.sh
+++ qgis-server/cmd.sh
@@ -3,6 +3,9 @@
 # Remplace les variables d'environnement dans le fichier template
 # envsubst < /data/project/dailymaps-template.qgs > /data/project/dailymaps.qgs
 
+# Remplace les chemins relatifs par des chemins absolus
+# sed -i 's#value="\.\./data/img#value="/data/img#g' /data/project/dailymaps.qgs
+
 [[ $DEBUG == "1" ]] && env
 
 exec /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
