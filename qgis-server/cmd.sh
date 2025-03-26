aa19784ea72d2bd00268c65b01a2d658a6b588cc
Ghassen Aouinti <ghassen.aouinti@gmail.com>
2025-03-26 12:56:03 -0400

    modify qgs files path

--- qgis-server/cmd.sh
+++ qgis-server/cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remplace les variables d'environnement dans le fichier template
-# envsubst < /data/dailymaps-template.qgs > /data/dailymaps.qgs
+# envsubst < /data/project/dailymaps-template.qgs > /data/project/dailymaps.qgs
 
 [[ $DEBUG == "1" ]] && env
 
